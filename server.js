36c8586b3a6d7b882ddfdbc19740f38a96e3c4fd
MushroomDevs-crypto <mushroomdevs@hotmail.com>
2025-05-11 16:40:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
